1ca0d2847784b6a7b8dd8ae39102b9ad20307a63
Ben Doan <ben@bendoan.me>
2015-04-15 02:35:08 +0000

    updated service

--- account_service.py
+++ account_service.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python2
+"""
+This script creates a small web service that allows for remote account creation. Make sure to change the conn info in insert_user before using.
+
+Requires: flask, MySQL-python
+
+Example usage: curl --data "username=flasktest&password=password&token=acmsecret" bdo.pw:5000/user/add -X POST
+"""
 
 from flask import *
 import sqlite3
@@ -9,23 +16,29 @@ app = Flask(__name__)
 
 def insert_user(username, password, name="NULL", email="NULL"):
     try:
-        conn = mdb.connect('localhost', 'admin', 'changemepass', 'domjudge')
+        conn = mdb.connect('localhost', 'root', 'testpass', 'domjudge')
         cur = conn.cursor()
 
         hashed_password = username + "#" + password
 
-        cur.execute("INSERT INTO domjudge.user (userid, username, name, email, last_login, last_ip_address, password, ip_address, enabled, teamid) VALUES (NULL, {}, {}, md5({}), NULL, NULL, {}, NULL, '1', NULL);".format(username, name, email, hashed_password))
-    except mdb.Error, e:
-        return "Error"
+	insert_user_query = """INSERT INTO 
+				domjudge.user (userid, username, name, email, last_login, last_ip_address, password, ip_address, enabled, teamid) 
+				VALUES (NULL, '{}', '{}', md5('{}'), NULL, NULL, '{}', NULL, '4', NULL);""".format(username, name, email, hashed_password)
 
-    print "Success"
+        cur.execute(insert_user_query)
+	conn.commit()
+    except mdb.Error, e:
+        return "Error: %s" % e
+
+    return "Success"
 
 @app.route('/user/add', methods=['POST'])
 def hello_world():
-    if request.form.get("username") request.form.get("password"):
-        return insert_user(request.form.get("username"), request.form.get("password"))
+    if request.form.get("username") and request.form.get("password") and request.form.get("token"):
+	if request.form.get("token") == "acmsecret":
+	    return insert_user(request.form.get("username"), request.form.get("password"))
     else:
         abort(418)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
